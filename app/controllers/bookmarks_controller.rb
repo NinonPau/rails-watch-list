4abe4b41bb02e38144bd00c6e33f4fc442342f11
Ninon Pau <ninon.pau@protonmail.com>
2025-08-24 18:34:26 +0200

    Fix the unnecessary new in :set_list only array which prevented the new bookmark/movie to be added to show view.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: %i[new create]
+  before_action :set_list, only: %i[create]
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
